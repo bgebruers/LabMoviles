6fdbe94aeddd9c3931b4f0be30789e55c150d707
bgebruers <blas.gebruers@gmail.com>
2023-09-04 19:06:18 -0300

    mensaje de funcion correcta de camara

--- Soporte/app/src/main/java/com/example/soporte/Camara.kt
+++ Soporte/app/src/main/java/com/example/soporte/Camara.kt
@@ -28,7 +28,9 @@ import java.io.FileOutputStream
 
 class Camara : AppCompatActivity() {
 
+    //Contiene la configuración para el hardware de captura
     lateinit var capReq: CaptureRequest.Builder
+    //Un controlador le permite enviar y procesar Message
     lateinit var handler: Handler
     lateinit var handlerThread: HandlerThread
     lateinit var cameraManager: CameraManager
@@ -52,14 +54,21 @@ class Camara : AppCompatActivity() {
         handlerThread.start()
         handler = Handler(handlerThread.looper)
 
+
+        //para recibir el párametro id desde el main activity
         val bundle= intent.extras
         val idCam= bundle?.getInt("idCam")
 
+
+        //texture view es para mostrar la imagen que captura la camara en pantalla
         textureView.surfaceTextureListener=object:TextureView.SurfaceTextureListener{
+
+            //cuando la texture view esta disponible se abre la camara
             override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
                 if (idCam != null) {
                     try{
                         open_camera(idCam)
+
                     }catch (e: Exception) {
                         Toast.makeText(
                             this@Camara,
@@ -84,7 +93,7 @@ class Camara : AppCompatActivity() {
             }
         }
 
-
+        //esto es para guardar la imagen capturada en el celu
         imageReader = ImageReader.newInstance(720, 1280, ImageFormat.JPEG, 1)
         imageReader.setOnImageAvailableListener(object:ImageReader.OnImageAvailableListener{
             override fun onImageAvailable(p0: ImageReader?) {
@@ -97,11 +106,8 @@ class Camara : AppCompatActivity() {
                 var opStream = FileOutputStream(file)
 
                 opStream.write(bytes)
-
-
                 opStream.close()
                 image.close()
-
 
                 Toast.makeText(this@Camara, "image capture", Toast.LENGTH_SHORT).show()
             }
@@ -117,13 +123,18 @@ class Camara : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint("MissingPermission")          //se da missing permision porque ya fueron verificados todos
     fun open_camera(idCam: Int){
         cameraManager.openCamera(cameraManager.cameraIdList[idCam], object:CameraDevice.StateCallback(){
             override fun onOpened(p0: CameraDevice) {
+                Toast.makeText(
+                    this@Camara,
+                    "La camara funciona de manera correcta",
+                    Toast.LENGTH_SHORT
+                ).show()
                 cameraDevice = p0
-                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                var surface = Surface(textureView.surfaceTexture)
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)   //solicitud de captura
+                var surface = Surface(textureView.surfaceTexture)       //obtiene la superficie del texture view
                 capReq.addTarget(surface)
 
                 cameraDevice.createCaptureSession(listOf(surface, imageReader.surface), object:CameraCaptureSession.StateCallback(){
@@ -149,18 +160,25 @@ class Camara : AppCompatActivity() {
         }, handler)
     }
     fun get_Permission(){
+        //permisos a pedir
         var permissionList = mutableListOf<String>()
+
+
+        //si el permiso no es otorgado se agrega a la lista para ser pedido
         if(checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
             permissionList.add(android.Manifest.permission.CAMERA)
         if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
             permissionList.add((android.Manifest.permission.READ_EXTERNAL_STORAGE))
         if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
             permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        //si la lista de permisos es mayor que 0 se piden los permisos
         if(permissionList.size > 0){
             requestPermissions(permissionList.toTypedArray(), 101)
         }
     }
 
+    //recorre los permisos para ver si todos fueron otorgados, sino lo fueron se llama de nuevo al getpermission
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
